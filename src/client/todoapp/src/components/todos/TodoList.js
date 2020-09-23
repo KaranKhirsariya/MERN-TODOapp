0c758d4c1a6a3572287db6a65a99c7d2a4f814f6
Karan Khirsariya <karan.khirsariya@gmail.com>
2020-09-24 01:44:57 +0530

    Add ToDo edit functionality

--- src/client/todoapp/src/components/todos/TodoList.js
+++ src/client/todoapp/src/components/todos/TodoList.js
@@ -2,33 +2,44 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './TodoList.css';
 import TodoListItem from './TodoListItem'
+import TodoForm from './TodoForm'
 
 function TodoList() {
-  
+
   const [todos, setTodos] = useState([])
-  const [todoItem, setTodoItem] = useState("");
+  const [editId, setEdit] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  const addItem = async event => {
-    event.preventDefault();
-    const item = {
-      title: todoItem,
-      description: null,
-      createdDate: new Date(),
-      completedDate: null,
-      dueDate: null,
-      done: false
-    }
-
+  const addTodo = (item) => {
     axios.post("/api/todos", item)
-      .then(res => {
-        console.log(res);
-        setTodos([...todos, res.data]);
-      })
-      .catch((err) => { 
-        console.error(err) 
-      });
+    .then(res => {
+      console.log(res);
+      setTodos([...todos, res.data]);
+    })
+    .catch((err) => {
+      console.error(err)
+    });
+  };
 
-    setTodoItem("");
+  const handleEditChange = (id, text) => {
+    setEdit(id);
+    setInputValue(text);
+  };
+
+  const editTodo = (id, text) => {
+    let editTodos = todos.map((todo) => {
+      if (todo._id === id) {
+        todo.title = text;
+      }
+      return todo;
+    });
+    setTodos(editTodos);
+    setEdit(false);
+    axios.patch("/api/todos/" + id, {title:text})
+      .catch(err => {
+        console.error(err);
+        getTodos();
+      })
   };
 
   useEffect(() => {
@@ -44,7 +55,7 @@ function TodoList() {
     }
   }
 
-  const handleDelete = (e, id) => {
+  const handleDelete = (id) => {
     const itemIndex = todos.findIndex(item => item._id === id)
     todos.splice(itemIndex, 1)
     axios.delete('/api/todos/' + id)
@@ -56,22 +67,24 @@ function TodoList() {
 
   }
 
-  const handleDone = (e, id) => {
+  const handleDone = (id) => {
     const item = todos.find((item) => item._id === id);
     item.done = !item.done;
+    setTodos([...todos])
     item.completedDate = (item.done) ? new Date() : null
     axios.patch('/api/todos/' + id, { done: item.done, completedDate: item.completedDate })
-      .then(res => {
-        setTodos([...todos]);
-      })
-      .catch(err => console.error(err));
+      .catch(err => {
+        console.error(err);
+        getTodos();
+      });
   }
 
   return (
     <div>
       <h2>Todos</h2>
       <hr />
-      <form onSubmit={addItem}>
+      <TodoForm onSubmit={addTodo} />
+      {/* <form onSubmit={addItem}>
         <label>
           <input
             name="item"
@@ -81,10 +94,19 @@ function TodoList() {
           />
         </label>
         <button type="submit">Add Todo</button>
-      </form>
+      </form> */}
       <ul className="todolist-items">
         {todos.map((todo) => {
-          return (<TodoListItem key={todo._id} data={todo} onDelete={handleDelete} onDone={handleDone} />)
+          return (<TodoListItem
+            key={todo._id}
+            todo={todo}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            editId={editId}
+            handleEditChange={handleEditChange}
+            editTodo={editTodo}
+            onDelete={handleDelete}
+            onDone={handleDone} />)
         })}
       </ul>
     </div>
--- src/client/todoapp/src/components/todos/TodoListItem.js
+++ src/client/todoapp/src/components/todos/TodoListItem.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ClearLogo from '../../resources/icons/clear.svg'
 
-function TodoListItem(props) {
+function TodoListItem({
+    todo,
+    onDone,
+    onDelete,
+    editTodo,
+    editId,
+    handleEditChange,
+    inputValue,
+    setInputValue }) {
+
+    const itemDisplay = () => {
+        return (editId === todo._id) ? (
+            <div className="TodoListItem__text--editing">
+                <input
+                    type="text"
+                    value={inputValue}
+                    
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button onClick={() => editTodo(todo._id, inputValue)}>Edit todo</button>
+            </div>
+        ) :
+            (<div>
+                <input type='checkbox' checked={todo.done} onChange={() => onDone(todo._id)}/>
+                <span className="TodoListItem__text" onClick={(e) => handleEditChange(todo._id, todo.title)}>{todo.title}</span>
+                <button className="TodoListItem__delete" onClick={() => onDelete(todo._id)} >
+                    <img src={ClearLogo} alt="Clear item" style={{ height: '16px', width: '16px' }} />
+                </button>
+            </div>)
+    };
+
     return (
-    <li key={props.data._id} className={'TodoListItem ' + (props.data.done ? 'done' : 'not-done')} id={props.data._id}>
-        <button className="TodoListItem__text" onClick={(e) => props.onDone(e,props.data._id)}>{props.data.title}</button>
-        <button className="TodoListItem__delete" onClick={(e) => props.onDelete(e,props.data._id)} ><img src={ClearLogo} alt="Clear item" style={{height:'16px', width: '16px'}}/></button>
-    </li>
+        <li key={todo._id} className={'TodoListItem ' + (todo.done ? 'done' : 'not-done')} id={todo._id}>
+
+            {itemDisplay()}
+
+        </li>
     )
 }
 
