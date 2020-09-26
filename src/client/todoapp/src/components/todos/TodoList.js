a10cee726dc6fe26cd26a09a9eda91d3489c5324
Karan Khirsariya <karan.khirsariya@gmail.com>
2020-09-27 02:21:44 +0530

    use react-editext component in TodoListItem

--- src/client/todoapp/src/components/todos/TodoList.js
+++ src/client/todoapp/src/components/todos/TodoList.js
@@ -7,40 +7,6 @@ import TodoForm from './TodoForm'
 function TodoList() {
 
   const [todos, setTodos] = useState([])
-  const [editId, setEdit] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-
-  const addTodo = (item) => {
-    axios.post("/api/todos", item)
-    .then(res => {
-      console.log(res);
-      setTodos([...todos, res.data]);
-    })
-    .catch((err) => {
-      console.error(err)
-    });
-  };
-
-  const handleEditChange = (id, text) => {
-    setEdit(id);
-    setInputValue(text);
-  };
-
-  const editTodo = (id, text) => {
-    let editTodos = todos.map((todo) => {
-      if (todo._id === id) {
-        todo.title = text;
-      }
-      return todo;
-    });
-    setTodos(editTodos);
-    setEdit(false);
-    axios.patch("/api/todos/" + id, {title:text})
-      .catch(err => {
-        console.error(err);
-        getTodos();
-      })
-  };
 
   useEffect(() => {
     getTodos();
@@ -55,15 +21,37 @@ function TodoList() {
     }
   }
 
+  const addTodo = (item) => {
+    axios.post("/api/todos", item)
+    .then(res => {
+      setTodos([...todos, res.data]);
+    })
+    .catch(console.error);
+  };
+
+  const editTodo = (text,{id}) => {
+    let editTodos = todos.map((todo) => {
+      if (todo._id === id) {
+        todo.title = text;
+      }
+      return todo;
+    });
+    setTodos(editTodos);
+
+    axios.patch("/api/todos/" + id, {title:text})
+      .catch(err => {
+        console.error(err);
+        getTodos();
+      })
+  };
+
   const handleDelete = (id) => {
     const itemIndex = todos.findIndex(item => item._id === id)
     todos.splice(itemIndex, 1)
     axios.delete('/api/todos/' + id)
       .then(() => {
         setTodos([...todos]);
-      }).catch(err => {
-        console.error(err);
-      })
+      }).catch(console.error);
 
   }
 
@@ -84,26 +72,12 @@ function TodoList() {
       <h2>Todos</h2>
       <hr />
       <TodoForm onSubmit={addTodo} />
-      {/* <form onSubmit={addItem}>
-        <label>
-          <input
-            name="item"
-            type="text"
-            value={todoItem}
-            onChange={e => setTodoItem(e.target.value)}
-          />
-        </label>
-        <button type="submit">Add Todo</button>
-      </form> */}
+      
       <ul className="todolist-items">
         {todos.map((todo) => {
           return (<TodoListItem
             key={todo._id}
             todo={todo}
-            inputValue={inputValue}
-            setInputValue={setInputValue}
-            editId={editId}
-            handleEditChange={handleEditChange}
             editTodo={editTodo}
             onDelete={handleDelete}
             onDone={handleDone} />)
